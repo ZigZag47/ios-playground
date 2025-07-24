d0a0a29e48e6ffad66b5103edecffdfdcbe489e1
Gabriel Alexandru <gabriel.alexandru@cst.ro>
2025-07-24 22:16:19 +0300

    Setup package.swift.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+import PackageDescription
+
+let package = Package(
+    name: "sdk_ios",
+    platforms: [
+        .iOS(.v18.4)
+    ],
+    products: [
+        .library(
+            name: "sdk_ios",
+            targets: ["sdk_ios"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "sdk_ios",
+            url: "https://zigzag47.github.io/ios-playground/sdk_ios.xcframework.zip",
+            checksum: "2b6b97957cd6a7b35cd55546bfa4f75088190e4dc03ee719e2958a342e82c496"
+        )
+    ]
+)
+
